63e341577098d1aac707ac6f7572b7143595da33
xieyuw <yuweix@umich.edu>
2016-10-20 22:24:42 -0400

    add logout.js

---
+++ static/js/logout.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('button').click(function(){
+    $.ajax({
+      url: '/hd1qzdkp/p3/api/v1/logout',
+      type: 'POST',
+      data: {},
+      success: function(response){
+      console.log(response);
+     $(location).attr('href','');
+      
+      },
+      error: function(error){
+      console.log(error);
+      }
+    });
+  });
+});
