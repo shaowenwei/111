cc8f0a808cd8e3d844b94ac29a1e43d2003c831c
xieyuw <yuweix@umich.edu>
2016-10-20 20:31:09 -0400

    Add files via upload

---
+++ static/js/login.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('button').click(function(){
+    var username = $('#login_username_input').val();
+    var password = $('#login_password_input').val();
+    $.ajax({
+      url: '/hd1qzdkp/p3/api/v1/login',
+      data: $('form').serialize(),
+      type: 'POST',
+      success: function(response){
+        res = JSON.parse(response);
+      
+      if(res['error'].username)
+      {
+        $(location).attr('href','albums');
+      }
+      else
+      {$("#errors").text(res['error'].errors[0].message);}
+      
+      },
+      error: function(error){
+      console.log(error);
+      }
+    });
+  });
+});
